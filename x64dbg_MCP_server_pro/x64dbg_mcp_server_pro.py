2410c598fedf478da49607d08fe9cb5aae9e2f38
Kanan H Akman <37771313+kenanhakman@users.noreply.github.com>
2025-06-04 16:09:12 +0300

    Download
    You can download the full version from the release section.

---
+++ x64dbg_MCP_server_pro/x64dbg_mcp_server_pro.py
@@ -0,0 +1,15 @@
+# /// script
+# requires-python = ">=3.10"
+# dependencies = [
+#     "requests>=2,<3",
+#     "mcp>=1.2.0,<2",
+# ]
+# ///
+
+import sys
+import json
+import requests
+from typing import Dict, List, Union, Optional, Any
+from dataclasses import dataclass
+
+The full code is in the release section.
